5eddd93275a0bfe16e21d544333a57411f13b98d
Yiweiwang <yiweiwangau@gmail.com>
2024-06-15 14:27:11 +1000

    Project initialization

---
+++ src/main/java/com/personal/Suduko/SudukoApplication.java
@@ -0,0 +1,13 @@
+package com.personal.Suduko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudukoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudukoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/Suduko/SudukoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.Suduko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudukoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
